f662056873d29a0e0180a4ccc5e0fc683e1e084f
alivarastepour <alivaraste@outlook.com>
2023-04-17 02:38:09 +0430

    new font weight loaded

--- Components/LandingPage/LandingPage.tsx
+++ Components/LandingPage/LandingPage.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/LandingPage/landingPage.module.scss";
 import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], weight: ["200"] });
 
 const LandingPage = () => {
   return (
