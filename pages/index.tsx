ad81db5597fa0a3a6ec24c9a82feacc5d2e38f64
alivarastepour <alivaraste@outlook.com>
2023-04-18 15:05:34 +0430

    document header added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import LandingPage from "@/Components/LandingPage/LandingPage";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Chronos: a motto</title>
+      </Head>
       <LandingPage />
     </>
   );
