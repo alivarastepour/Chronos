d974476e9eaab983b7acff6d54e190ce0af1e4c3
alivarastepour <alivaraste@outlook.com>
2023-04-18 15:05:56 +0430

    Benefits component added

---
+++ Components/Benefits/Benefits.tsx
@@ -0,0 +1,33 @@
+import Card from "../Card/Card";
+
+const Benefits: React.FC = () => {
+  return (
+    <>
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+      <Card
+        header="Lorem ipsum dolor sit amet"
+        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+      />
+    </>
+  );
+};
+export default Benefits;
