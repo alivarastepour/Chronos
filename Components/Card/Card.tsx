97a35036b57b0475a969d6b62967102236a87911
alivarastepour <alivaraste@outlook.com>
2023-04-18 17:34:43 +0430

    slide effect added to the Card component

--- Components/Card/Card.tsx
+++ Components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/Card/card.module.scss";
 import { NextFont } from "@next/font";
 import { Inter } from "@next/font/google";
-const inter: NextFont = Inter({ subsets: ["latin"], weight: ["300"] });
+const inter: NextFont = Inter({ subsets: ["latin"], weight: ["200", "500"] });
 
 interface CardProps {
   header: string;
@@ -16,9 +16,12 @@ const Card: React.FC<CardProps> = ({ header, explaination }) => {
         className={`${styles["card-container"]} ${inter.className}`}
       >
         <div className={styles["card-header"]}>{header}</div>
-        <div className={styles["card-explaination"]}>{explaination}</div>
-        <div className={styles["card-show-more-container"]}>
-          <div className={styles["show-more-button"]}>Read More</div>
+        <div className={styles["overlay"]}>
+          <div className={styles["card-explaination"]}>
+            <div className={styles["card-explaination-wrapper"]}>
+              {explaination}
+            </div>
+          </div>
         </div>
       </div>
     </>
