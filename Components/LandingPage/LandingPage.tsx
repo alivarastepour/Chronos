86d963f72a89fe920a4b84f95488f17cad5505c7
alivarastepour <alivaraste@outlook.com>
2023-04-18 14:19:48 +0430

    landing page subtitle is added

--- Components/LandingPage/LandingPage.tsx
+++ Components/LandingPage/LandingPage.tsx
@@ -1,15 +1,25 @@
 import styles from "@/styles/LandingPage/landingPage.module.scss";
 import { Inter } from "@next/font/google";
+import Benefits from "../Benefits/Benefits";
+import { NextFont } from "@next/font";
 
-const inter = Inter({ subsets: ["latin"], weight: ["200"] });
+const inter: NextFont = Inter({ subsets: ["latin"], weight: ["300"] });
 
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
   return (
     <>
-      <div
-        className={`${styles["landing-title-container"]} ${inter.className}`}
-      >
-        <div className={styles["landing-title"]}>Chronos</div>
+      <div className={`${styles["landing-title-container"]} `}>
+        <div className={`${styles["landing-title"]} ${inter.className}`}>
+          Chronos
+        </div>
+      </div>
+      <div className={styles["landing-subtitle-container"]}>
+        <div className={`${styles["landing-subtitle"]} ${inter.className}`}>
+          Manage Your Time Like You Are The God Of It
+        </div>
+      </div>
+      <div className={styles["benefits-container"]}>
+        <Benefits />
       </div>
     </>
   );
