b5bd1acd87ff768f66160a54b2efad82b0b47c5c
alivarastepour <alivaraste@outlook.com>
2023-04-18 15:06:08 +0430

    initial UI design for Card component is added

---
+++ Components/Card/Card.tsx
@@ -0,0 +1,21 @@
+import styles from "@/styles/Card/card.module.scss";
+import { NextFont } from "@next/font";
+import { Inter } from "@next/font/google";
+const inter: NextFont = Inter({ subsets: ["latin"], weight: ["300"] });
+
+interface CardProps {
+  header: string;
+  explaination: string;
+}
+
+const Card: React.FC<CardProps> = ({ header, explaination }) => {
+  return (
+    <>
+      <div className={`${styles["card-container"]} ${inter.className}`}>
+        <div className={styles["card-header"]}>{header}</div>
+        <div className={styles["card-explaination"]}>{explaination}</div>
+      </div>
+    </>
+  );
+};
+export default Card;
