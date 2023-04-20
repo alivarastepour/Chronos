89f645951f767b46ebfa3f7d53c3422702eee384
alivarastepour <alivaraste@outlook.com>
2023-04-20 21:01:11 +0430

    more details for Overview component is added

--- Components/LandingPage/LandingPage.tsx
+++ Components/LandingPage/LandingPage.tsx
@@ -1,22 +1,9 @@
-import styles from "@/styles/LandingPage/landingPage.module.scss";
-import { inter } from "@/public/Fonts";
-
 import Benefits from "../Benefits/Benefits";
 import Overview from "../Overview/Overview";
 
 const LandingPage: React.FC = () => {
   return (
     <>
-      <div className={`${styles["landing-title-container"]} `}>
-        <div className={`${styles["landing-title"]} ${inter.className}`}>
-          Chronos
-        </div>
-      </div>
-      <div className={styles["landing-subtitle-container"]}>
-        <div className={`${styles["landing-subtitle"]} ${inter.className}`}>
-          Manage Your Time Like You Are The God Of It
-        </div>
-      </div>
       <Overview />
 
       <Benefits />
--- Components/Overview/Overview.tsx
+++ Components/Overview/Overview.tsx
@@ -1,15 +1,21 @@
+import Image from "next/image";
+
 import styles from "@/styles/Overview/overview.module.scss";
 import { inter } from "@/public/Fonts";
+
 import HeroImage from "@/public/TrelloUICollage_4x.webp";
-import Image from "next/image";
+
 const Overview: React.FC = () => {
   return (
     <>
       <div className={styles["overview-container"]}>
         <div className={`${styles["left-overview"]} ${inter.className}`}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt impedit
-          dolorem, perferendis nam dolorum fuga velit! Nisi, corrupti? Est,
-          minus!
+          <div className={`${styles["landing-title"]} ${inter.className}`}>
+            Chronos
+          </div>
+          <div className={`${styles["landing-subtitle"]} ${inter.className}`}>
+            Manage Your Time Like You Are The God Of It
+          </div>
         </div>
         <div className={styles["right-overview"]}>
           <Image
