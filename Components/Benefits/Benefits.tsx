4e6d3fd4bf280eb9ead47897dbe6e214d563418d
alivarastepour <alivaraste@outlook.com>
2023-04-20 19:31:07 +0430

    section containers are moved to inside of components

--- Components/Benefits/Benefits.tsx
+++ Components/Benefits/Benefits.tsx
@@ -1,32 +1,34 @@
 import Card from "../Card/Card";
-
+import styles from "@/styles/Benefits/benefits.module.scss";
 const Benefits: React.FC = () => {
   return (
     <>
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
-      <Card
-        header="Lorem ipsum dolor sit amet"
-        explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-      />
+      <div className={styles["benefits-container"]}>
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+        <Card
+          header="Lorem ipsum dolor sit amet"
+          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
+        />
+      </div>
     </>
   );
 };
