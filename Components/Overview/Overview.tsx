546e657bf6a753ff545575625b51bd886a7556c2
alivarastepour <alivaraste@outlook.com>
2023-04-21 21:08:43 +0430

    more color adjustemnt

--- Components/Overview/Overview.tsx
+++ Components/Overview/Overview.tsx
@@ -8,25 +8,27 @@ import HeroImage from "@/public/TrelloUICollage_4x.webp";
 const Overview: React.FC = () => {
   return (
     <>
-      <div className={styles["overview-container"]}>
-        <div className={`${styles["left-overview"]} ${inter.className}`}>
-          <div className={`${styles["landing-title"]} ${inter.className}`}>
-            Chronos
+      <div className={styles["overview-wrapper"]}>
+        <div className={styles["overview-container"]}>
+          <div className={`${styles["left-overview"]} ${inter.className}`}>
+            <div className={`${styles["landing-title"]} ${inter.className}`}>
+              Chronos
+            </div>
+            <div className={`${styles["landing-subtitle"]} ${inter.className}`}>
+              Manage Your Time Like You Are The God Of It
+            </div>
           </div>
-          <div className={`${styles["landing-subtitle"]} ${inter.className}`}>
-            Manage Your Time Like You Are The God Of It
+          <div className={styles["right-overview"]}>
+            <Image
+              src={HeroImage}
+              width={630}
+              height={560}
+              quality={100}
+              placeholder="blur"
+              alt="managing tasks"
+              priority
+            />
           </div>
-        </div>
-        <div className={styles["right-overview"]}>
-          <Image
-            src={HeroImage}
-            width={630}
-            height={560}
-            quality={100}
-            placeholder="blur"
-            alt="managing tasks"
-            priority
-          />
         </div>
       </div>
     </>
