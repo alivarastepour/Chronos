60762bbf292b9927591103b446e446be2143495d
alivarastepour <alivaraste@outlook.com>
2023-04-22 18:41:31 +0430

    intial styles of Benefits component is added

--- Components/Benefits/Benefits.tsx
+++ Components/Benefits/Benefits.tsx
@@ -1,33 +1,41 @@
 import Card from "../Card/Card";
 import styles from "@/styles/Benefits/benefits.module.scss";
+import { inter } from "@/public/Fonts";
 const Benefits: React.FC = () => {
   return (
     <>
-      <div className={styles["benefits-container"]}>
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
-        <Card
-          header="Lorem ipsum dolor sit amet"
-          explaination="Lorem ipsum dolor sit amet, eos ut virtute numquam, eu cibo democritum cum. Nam ubique ignota an. Cu vel malis solet tincidunt, voluptua interesset sed ne. Elitr semper noluisse id mea. No vitae graeci facete usu."
-        />
+      <div className={`${styles["benefits-container"]} ${inter.className}`}>
+        <div className={styles["benefits-title"]}>
+          Lorem ipsum dolor sit, amet consectetur.
+        </div>
+        <div className={styles["benefits-content"]}>
+          <div className={styles["x"]}>
+            <div>
+              1Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Corrupti saepe excepturi inventore. Sed, adipisci reiciendis?
+              Tempore, asperiores, nobis itaque totam quis possimus repellat
+              alias quasi
+            </div>
+            <div>
+              2Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Corrupti saepe excepturi inventore. Sed, adipisci reiciendis?
+              Tempore, asperiores, nobis itaque totam quis possimus repellat
+              alias quasi
+            </div>
+            <div>
+              3Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Corrupti saepe excepturi inventore. Sed, adipisci reiciendis?
+              Tempore, asperiores, nobis itaque totam quis possimus repellat
+              alias quasi
+            </div>
+            <div>
+              4Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Corrupti saepe excepturi inventore. Sed, adipisci reiciendis?
+              Tempore, asperiores, nobis itaque totam quis possimus repellat
+              alias quasi
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
