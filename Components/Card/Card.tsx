d64ad04152253945882f324ed130b0e71f66f516
alivarastepour <alivaraste@outlook.com>
2023-04-26 12:43:59 +0430

    Card component extended using Compound pattern

--- Components/Card/Card.tsx
+++ Components/Card/Card.tsx
@@ -2,24 +2,44 @@ import styles from "@/styles/Card/card.module.scss";
 import { inter } from "@/public/Fonts";
 
 interface CardProps {
-  // header: string;
-  explaination: string;
+  children: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ explaination }) => {
+const CardHeader: React.FC<CardProps> = ({ children }) => {
   return (
     <>
-      <div className={`${styles["card-container"]} ${inter.className}`}>
-        {/* <div className={styles["card-header"]}>{header}</div> */}
-        <div className={styles["overlay"]}>
-          <div className={styles["card-explaination"]}>
-            <div className={styles["card-explaination-wrapper"]}>
-              {explaination}
-            </div>
-          </div>
+      <div className={styles["card-header"]}>{children}</div>
+    </>
+  );
+};
+const CardExplaination: React.FC<CardProps> = ({ children }) => {
+  return (
+    <>
+      <div className={styles["overlay"]}>
+        <div className={styles["card-explaination"]}>
+          <div className={styles["card-explaination-wrapper"]}>{children}</div>
         </div>
       </div>
     </>
   );
 };
+
+interface CardComposition {
+  Explaination: typeof CardExplaination;
+  Header: typeof CardHeader;
+}
+
+const Card: React.FC<CardProps> & CardComposition = ({ children }) => {
+  return (
+    <>
+      <div className={`${styles["card-container"]} ${inter.className}`}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+Card.Explaination = CardExplaination;
+Card.Header = CardHeader;
+
 export default Card;
