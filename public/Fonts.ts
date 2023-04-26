b818908e70bced135726fa019a42838bdb5f4bf5
alivarastepour <alivaraste@outlook.com>
2023-04-26 13:13:16 +0430

    font weights are described more precisely

--- public/Fonts.ts
+++ public/Fonts.ts
@@ -2,5 +2,5 @@ import { Inter } from "@next/font/google";
 import { NextFont } from "@next/font";
 export const inter: NextFont = Inter({
   subsets: ["latin"],
-  weight: ["200", "300"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
