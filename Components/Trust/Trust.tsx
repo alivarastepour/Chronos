e48f5f7bd5f6480d67564f4228418140aa5b3f1d
alivarastepour <alivaraste@outlook.com>
2023-04-29 23:25:26 +0430

    first row of Trust component is initialized

--- Components/Trust/Trust.tsx
+++ Components/Trust/Trust.tsx
@@ -4,15 +4,39 @@ import styles from "@/styles/Trust/trust.module.scss";
 const Trust = () => {
   return (
     <>
-      <div className={styles["trust-container"]}>
-        {/* <div cl></div> */}
-        <div className={`${styles["trust-item"]} ${styles["grow-1"]}`}>1</div>
-        <div className={`${styles["trust-item"]} ${styles["grow-2"]}`}>2</div>
-        <div className={styles["trust-item"]}>3</div>
-        <div className={`${styles["trust-item"]} ${styles["grow-4"]}`}>4</div>
-        <div className={`${styles["trust-item"]} ${styles["grow-5"]}`}>5</div>
-        <div className={`${styles["trust-item"]} ${styles["grow-6"]}`}>6</div>
-        <div className={styles["trust-item"]}>7</div>
+      <div className={styles["trust-wrapper"]}>
+        <div className={`${styles["trust-container-1"]} ${inter.className}`}>
+          <div className={`${styles["trust-item"]} ${styles["item-1"]}`}>
+            <div className={styles["trust-content"]}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus, quibusdam. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Odit atque dicta consequatur? Excepturi culpa,
+              vel inventore odit amet voluptatibus repellat doloribus ab tempora
+              similique id. Corrupti vero magni perspiciatis ut.
+            </div>
+            <div className={styles["trust-quote"]}>- neil patrick harris</div>
+          </div>
+          <div className={`${styles["trust-item"]} ${styles["item-2"]}`}>
+            <div className={styles["trust-content"]}>
+              dolor sit amet consectetur adipisicing elit. Nam et architecto
+              fugit est voluptatum iure ducimus velit repellat incidunt
+              reprehenderit.
+            </div>
+            <div className={styles["trust-quote"]}>- jason sagal</div>
+          </div>
+          <div className={`${styles["trust-item"]} ${styles["item-3"]}`}>
+            <div className={styles["trust-content"]}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+              excepturi nostrum assumenda odit fugit non! Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Nisi, possimus. Lorem ipsum
+            </div>
+            <div className={styles["trust-quote"]}>- rhea seahorn</div>
+          </div>
+        </div>
+        <div className={`${styles["trust-item"]} ${styles["item-4"]}`}></div>
+        <div className={`${styles["trust-item"]} ${styles["item-5"]}`}></div>
+        <div className={`${styles["trust-item"]} ${styles["item-6"]}`}></div>
+        <div className={`${styles["trust-item"]} ${styles["item-7"]}`}></div>
       </div>
     </>
   );
