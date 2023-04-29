604db628e757c9d799b6cef141f55cb05bfd2893
alivarastepour <alivaraste@outlook.com>
2023-04-30 00:53:00 +0430

    hover effect is added for the trust component

--- Components/Trust/Trust.tsx
+++ Components/Trust/Trust.tsx
@@ -26,7 +26,10 @@ const Trust = () => {
             </div>
             <div className={styles["trust-quote"]}>- neil patrick harris</div>
           </div>
-          <div className={`${styles["trust-item"]} ${styles["item-2"]}`}>
+          <div
+            id="item-2"
+            className={`${styles["trust-item"]} ${styles["item-2"]}`}
+          >
             <div className={styles["trust-content"]}>
               dolor sit amet consectetur adipisicing elit. Nam et architecto
               fugit est voluptatum iure ducimus velit repellat incidunt
@@ -34,7 +37,10 @@ const Trust = () => {
             </div>
             <div className={styles["trust-quote"]}>- jason sagal</div>
           </div>
-          <div className={`${styles["trust-item"]} ${styles["item-3"]}`}>
+          <div
+            id="item-3"
+            className={`${styles["trust-item"]} ${styles["item-3"]}`}
+          >
             <div className={styles["trust-content"]}>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
               excepturi nostrum assumenda odit fugit non! Lorem ipsum dolor sit
@@ -46,7 +52,10 @@ const Trust = () => {
         <div
           className={`${styles["trust-container"]} ${styles["trust-container-2"]} ${inter.className}`}
         >
-          <div className={`${styles["trust-item"]} ${styles["item-4"]}`}>
+          <div
+            id="item-4"
+            className={`${styles["trust-item"]} ${styles["item-4"]}`}
+          >
             <div className={styles["trust-content"]}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Praesentium iure ex optio error similique accusamus, deleniti
@@ -54,7 +63,10 @@ const Trust = () => {
             </div>
             <div className={styles["trust-quote"]}>- rhea seahorn</div>
           </div>
-          <div className={`${styles["trust-item"]} ${styles["item-5"]}`}>
+          <div
+            id="item-5"
+            className={`${styles["trust-item"]} ${styles["item-5"]}`}
+          >
             <div className={styles["trust-content"]}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
               temporibus magni deserunt impedit. Facere veniam harum enim magnam
