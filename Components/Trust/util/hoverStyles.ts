05a7550394f9d8a4ba15ea8e54632842bb70d5fb
alivarastepour <alivaraste@outlook.com>
2023-04-30 12:50:14 +0430

    code cleanup and type checking added for more safety

--- Components/Trust/util/hoverStyles.ts
+++ Components/Trust/util/hoverStyles.ts
@@ -1,28 +1,18 @@
-let isThemeDark = false;
+let isThemeDark: boolean = false;
 
-let color = "";
+let color: string = "";
 
 export const addHoverStyle = () => {
-  const e1 = document.getElementById("item-1");
-  const e2 = document.getElementById("item-2");
-  const e3 = document.getElementById("item-3");
-  const e4 = document.getElementById("item-4");
-  const e5 = document.getElementById("item-5");
+  const elements: (HTMLElement | null)[] = [1, 2, 3, 4, 5, 6].map((num) =>
+    document.getElementById(`item-${num}`)
+  );
 
-  [e1, e2, e3, e4, e5].forEach((e) => {
-    e?.addEventListener("mousemove", (ee) => {
-      const rrect = e.getBoundingClientRect();
+  elements.forEach((element: HTMLElement | null) => {
+    element?.addEventListener("mousemove", (event: MouseEvent) => {
+      const elementRect: DOMRect = element.getBoundingClientRect();
 
-      var newCenterX = ee.clientX - rrect.left; //x position within the element.
-      var newCenterY = ee.clientY - rrect.top;
-
-      // const newCenterX = ee.pageX - e.offsetLeft;
-      // const newCenterY = ee.pageY - e.offsetTop;
-      console.log(newCenterX, newCenterY);
-
-      // var rect = ee.pageX - e.offsetLeft;
-      // var x = e.clientX - rect.left; //x position within the element.
-      // var y = e.clientY - rect.top; //y position within the element.
+      const newCenterX: number = event.clientX - elementRect.left;
+      const newCenterY: number = event.clientY - elementRect.top;
 
       isThemeDark =
         window.matchMedia &&
@@ -30,17 +20,17 @@ export const addHoverStyle = () => {
 
       if (isThemeDark) color = "rgba(25, 24, 37, 0.9) , #191825";
       else color = "#ffe5e5 , rgba(255, 229, 229, 0.1) ";
-      // else color = "red, yellow";
-      const radCol = color.split(",")[0];
-      e.style.background = "none";
-      e.style.backgroundImage = `radial-gradient(circle at ${newCenterX}px ${newCenterY}px, ${color})`;
+
+      element.style.background = "none";
+      element.style.backgroundImage = `radial-gradient(circle at ${newCenterX}px ${newCenterY}px, ${color})`;
     });
 
-    [e1, e2, e3, e4, e5].forEach((e) => {
-      e?.addEventListener("mouseleave", () => {
-        e.style.backgroundColor = color.split(",")[0];
-        // e.style.backgroundImage = "none";
+    elements.forEach((element: HTMLElement | null) => {
+      element?.addEventListener("mouseleave", () => {
+        element.style.backgroundColor = color.split(",")[0];
       });
     });
   });
 };
+
+
