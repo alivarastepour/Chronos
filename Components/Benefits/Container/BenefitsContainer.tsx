6d0fb9a833bf97e4745899750e22a4cae48e6d62
alivarastepour <alivaraste@outlook.com>
2023-05-04 01:53:27 +0430

    indicator change logic is added

--- Components/Benefits/Container/BenefitsContainer.tsx
+++ Components/Benefits/Container/BenefitsContainer.tsx
@@ -3,21 +3,71 @@ import { intersectionHandler } from "../utill/transformHandler";
 import BenefitsPresenter from "../presenter/BenefitsPresenter";
 
 const BenefitsContainer = () => {
+  const initIntersectionObserver = (
+    target: Element | null
+  ): IntersectionObserver | null => {
+    if (target == null) {
+      console.error("benefits section wrapper was not found.");
+      return null;
+    }
+    const options: IntersectionObserverInit = {
+      root: null,
+      threshold: 1.0,
+      rootMargin: "0px",
+    };
+    const intersectionOberver: IntersectionObserver = new IntersectionObserver(
+      intersectionHandler,
+      options
+    );
+    return intersectionOberver;
+  };
+
+  const indicatorInitializer = (indicator: Element | null): void => {
+    indicator?.addEventListener("click", (event) => {
+      const target: Element | null = event.target as Element;
+      const currentTarget: Element | null = event.currentTarget as Element;
+      if (Object.is(currentTarget, target)) {
+        return;
+      }
+      const className: DOMTokenList = target.classList;
+      if (className.contains("benefits-counter-active")) {
+        return;
+      } else {
+        target.classList.remove("benefits-counter-deactive");
+        target.classList.add("benefits-counter-active");
+        let ch: any = null;
+        indicator.childNodes.forEach((child) => {
+          if (ch === null) {
+            const childElem = child as Element;
+            ch = childElem.classList.contains("benefits-counter-active")
+              ? childElem
+              : null;
+          }
+        });
+        if (ch instanceof Element) {
+          ch.classList.remove("benefits-counter-active");
+          ch.classList.add("benefits-counter-deactive");
+        }
+      }
+    });
+  };
+
   useEffect(() => {
     const target: Element | null = document.querySelector("#benefit-container");
-    if (target !== null) {
-      const options: IntersectionObserverInit = {
-        root: null,
-        threshold: 1.0,
-        rootMargin: "0px",
-      };
-      const intersectionOberver: IntersectionObserver =
-        new IntersectionObserver(intersectionHandler, options);
-      intersectionOberver.observe(target);
-      () => {
-        intersectionOberver.disconnect();
-      };
+    const observer: any = initIntersectionObserver(target);
+    if (observer instanceof IntersectionObserver) {
+      // @ts-ignore if target is null then observer won't be an instance of IntersectionObsrrver
+      observer.observe(target);
     }
+
+    const indicator: Element | null =
+      document.getElementById("benefits-counter");
+
+    indicatorInitializer(indicator);
+
+    () => {
+      observer?.disconnect();
+    };
   }, []);
   return <BenefitsPresenter />;
 };
