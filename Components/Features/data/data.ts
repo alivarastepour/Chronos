12b92baf042ca7dfffb1a2b63e1a14743ad9f5f5
alivarastepour <alivaraste@outlook.com>
2023-05-09 11:40:55 +0430

    data file for Features component is added

---
+++ Components/Features/data/data.ts
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: 1,
+    title: "Manage your tasks in one shared place",
+    content:
+      "Say goodbye to sticky notes and to-do lists: Trello allows teams of any size to easily manage tasks and hit deadlines - all without ever leaving the app. Trello makes it easy to organize and track individual or team tasks in one place",
+    className: "features-selected",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repudiandae harum repellendus perspiciatis veniam ea quas quaerat dignissimos eligendi ipsa consequatur, ratione doloribus commodi suscipit. Provident inventore illo officiis perspiciatis?",
+    className: "features-not-selected",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repudiandae harum repellendus perspiciatis veniam ea quas quaerat dignissimos eligendi ipsa consequatur, ratione doloribus commodi suscipit. Provident inventore illo officiis perspiciatis?",
+    className: "features-not-selected",
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repudiandae harum repellendus perspiciatis veniam ea quas quaerat dignissimos eligendi ipsa consequatur, ratione doloribus commodi suscipit. Provident inventore illo officiis perspiciatis?",
+    className: "features-not-selected",
+  },
+  {
+    id: 5,
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repudiandae harum repellendus perspiciatis veniam ea quas quaerat dignissimos eligendi ipsa consequatur, ratione doloribus commodi suscipit. Provident inventore illo officiis perspiciatis?",
+    className: "features-not-selected",
+  },
+  {
+    id: 6,
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repudiandae harum repellendus perspiciatis veniam ea quas quaerat dignissimos eligendi ipsa consequatur, ratione doloribus commodi suscipit. Provident inventore illo officiis perspiciatis?",
+    className: "features-not-selected",
+  },
+];
