d0d5b5d75ecb3279d40ff1d0d93fb74698b9f7d6
alivarastepour <alivaraste@outlook.com>
2023-05-10 17:28:17 +0430

    close icon svg is added to CookiesTOS component

--- Components/CookiesTOS/CookiesTOS.tsx
+++ Components/CookiesTOS/CookiesTOS.tsx
@@ -5,7 +5,24 @@ const CookiesTOS: React.FC = () => {
   return (
     <>
       <div className={`${styles["cookie-container"]} ${inter.className}`}>
-        <div className={styles["cookie-header"]}>everyone loves a cookie</div>
+        <div className={styles["cookie-header"]}>
+          <div>everyone loves a cookie</div>
+          <div>
+            <svg
+              className={styles["close-btn"]}
+              id="close-btn"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 50 50"
+              fill="currentColor"
+            >
+              <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+            </svg>
+          </div>
+        </div>
         <div className={styles["cookie-body"]}>
           <div className={styles["cookie-text"]}>
             We use cookies and other tracking technologies to improve your
