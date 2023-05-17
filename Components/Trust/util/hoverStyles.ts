5b676f7aa26cc9c14aba02f0cb225ac5a324da74
alivarastepour <alivaraste@outlook.com>
2023-05-17 21:29:48 +0330

    hover styles of Trust section re-written with more readability and reusability

--- Components/Trust/util/hoverStyles.ts
+++ Components/Trust/util/hoverStyles.ts
@@ -1,21 +1,75 @@
-let isThemeDark: boolean = false;
-
 let color: string = "";
 
-const addFadingBackground = (elements: (HTMLElement | null)[]) => {
+type userTheme = "dark" | "light";
+
+const getUserTheme = (): userTheme => {
+  const isThemeDark: boolean =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const theme: userTheme = isThemeDark ? "dark" : "light";
+  return theme;
+};
+
+const getMousePosition = (
+  element: HTMLElement,
+  event: MouseEvent
+): [newCenterX: number, newCentetY: number] => {
+  const elementRect: DOMRect = element.getBoundingClientRect();
+
+  const newCenterX: number = event.clientX - elementRect.left;
+  const newCenterY: number = event.clientY - elementRect.top;
+
+  return [newCenterX, newCenterY];
+};
+
+const getContainerShape = (
+  element: HTMLElement
+): [width: number, height: number] => {
+  const elementRect: DOMRect = element.getBoundingClientRect();
+
+  const width: number = elementRect.right - elementRect.left;
+  const height: number = elementRect.bottom - elementRect.top;
+
+  return [width, height];
+};
+
+// since in our usecase we always know that our value is between 0 and 100, we have simplified the interpolation formula
+const linearInterpolationTransformer = (
+  min: number,
+  max: number,
+  value: number
+): number => {
+  return min + ((max - min) / 100) * value;
+};
+
+const mapToEdge = (
+  percentageX: number,
+  percentageY: number,
+  calculatedX: number,
+  calculatedY: number
+): [calculatedX: number, calculatedY: number] => {
+  if (percentageX >= 50) {
+    if (percentageY >= 50) calculatedY *= -1;
+    else {
+      calculatedY *= -1;
+      calculatedX *= -1;
+    }
+  } else {
+    if (percentageY < 50) calculatedX *= -1;
+  }
+  return [calculatedX, calculatedY];
+};
+
+const addFadingBackground = (elements: (HTMLElement | null)[]): void => {
   elements.forEach((element: HTMLElement | null) => {
     element?.addEventListener("mousemove", (event: MouseEvent) => {
-      const elementRect: DOMRect = element.getBoundingClientRect();
+      const [newCenterX, newCenterY]: [newCenterX: number, newCenterY: number] =
+        getMousePosition(element, event);
 
-      const newCenterX: number = event.clientX - elementRect.left;
-      const newCenterY: number = event.clientY - elementRect.top;
+      const theme: userTheme = getUserTheme();
 
-      isThemeDark =
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-      if (isThemeDark) color = "rgb(66, 64, 89) , rgba(25, 24, 37, 0.9)";
-      else color = " rgba(255, 229, 229, 0.1), #ffe5e5 ";
+      if (theme == "dark") color = "#424059, #191825e6";
+      else color = "#ffe5e51a, #ffe5e5";
 
       element.style.background = "none";
       element.style.backgroundImage = `radial-gradient(circle at ${newCenterX}px ${newCenterY}px, ${color})`;
@@ -30,44 +84,51 @@ const addFadingBackground = (elements: (HTMLElement | null)[]) => {
   });
 };
 
-const addRotation = (elements: (HTMLElement | null)[]) => {
+const addRotation = (elements: (HTMLElement | null)[]): void => {
   elements.forEach((element: HTMLElement | null) => {
     element?.addEventListener("mousemove", (event: MouseEvent) => {
-      const elementRect: DOMRect = element.getBoundingClientRect();
+      const [width, height]: [width: number, height: number] =
+        getContainerShape(element);
 
-      const height: number = elementRect.bottom - elementRect.top;
-      const width: number = elementRect.right - elementRect.left;
-
-      const hoverPositionX: number = event.clientX - elementRect.left;
-      const hoverPositionY: number = event.clientY - elementRect.top;
+      const [hoverPositionX, hoverPositionY]: [
+        hoverPositionX: number,
+        hoverPositionY: number
+      ] = getMousePosition(element, event);
 
       const percentageX: number = (100 * hoverPositionX) / width;
       const percentageY: number = (100 * hoverPositionY) / height;
 
-      let calculatedX: number = Math.abs(-1 + 0.02 * percentageX);
-      let calculatedY: number = Math.abs(-1.5 + 0.03 * percentageY);
+      // use linear interpolation to map a value between 0 and 100 to a desired range
 
-      if (percentageX >= 50) {
-        if (percentageY >= 50) calculatedY *= -1;
-        else {
-          calculatedY *= -1;
-          calculatedX *= -1;
-        }
-      } else {
-        if (percentageY < 50) calculatedX *= -1;
-      }
+      let [calculatedX, calculatedY]: [
+        calculatedX: number,
+        calculatedY: number
+      ] = [
+        Math.abs(linearInterpolationTransformer(-1, 1, percentageX)),
+        Math.abs(linearInterpolationTransformer(-1.5, 1.5, percentageY)),
+      ];
+
+      // interpolated values must map to 4 edges, so we use a combinations of actuall and negated values
+
+      [calculatedX, calculatedY] = mapToEdge(
+        percentageX,
+        percentageY,
+        calculatedX,
+        calculatedY
+      );
+
       element.style.transform = `perspective(1000px) rotateY(${calculatedY}deg) rotateX(${calculatedX}deg)`;
     });
   });
 
   elements.forEach((element: HTMLElement | null) => {
-    element?.addEventListener("mouseout", (event: MouseEvent) => {
+    element?.addEventListener("mouseout", () => {
       element.style.transform = "unset";
     });
   });
 };
 
-export const handleHoverStyles = () => {
+export const handleHoverStyles = (): void => {
   const elements: (HTMLElement | null)[] = [1, 2, 3, 4, 5, 6].map((num) =>
     document.getElementById(`item-${num}`)
   );
