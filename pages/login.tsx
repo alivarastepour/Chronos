de751371c141d2b03662059defb3aa97a5aaaae7
alivarastepour <alivaraste@outlook.com>
2023-05-19 17:56:01 +0330

    Login page inits

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Chronos | Login To Your Account</title>
+      </Head>
+      <div>hello world</div>
+    </>
+  );
+};
+
+export default Login;
