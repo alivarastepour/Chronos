80754bc213f37e22c942c41ad7d46dd5a40cac4f
alivarastepour <alivaraste@outlook.com>
2023-05-27 15:51:38 +0330

    utill functionality of Plans component partially re-written(has bugs)

--- Components/Plans/utill/focusOnHover.ts
+++ Components/Plans/utill/focusOnHover.ts
@@ -69,13 +69,99 @@ const changeEffect = (event: Event): void => {
   }
 };
 
-export const focusOnHover = (): void => {
+type directionKeys = "1" | "2" | "3";
+type directionValues = "left" | "mid" | "right";
+
+type changeDirection = {
+  [key in directionKeys]: directionValues;
+};
+
+const getChangeDirection = (
+  targetPosition: keyof changeDirection
+): directionValues => {
+  const positionMap: changeDirection = { 1: "right", 2: "mid", 3: "left" };
+  return positionMap[targetPosition];
+};
+
+const getOppositeDirection = (dir: directionValues) => {
+  switch (dir) {
+    case "left":
+      return "right";
+    case "mid":
+      return "mid";
+    case "right":
+      return "left";
+  }
+};
+
+const focusOnTarget = (target: Element, dir: directionValues) => {
+  target.classList.add(`active-${dir}`);
+  target.classList.remove(`deactive-${getOppositeDirection(dir)}`);
+};
+
+const removeFocusOnPrevTarget = (
+  parent: HTMLElement,
+  changeDirection: directionValues
+) => {
+  // parent.childNodes.forEach((child) => {
+  //   const childClassList: DOMTokenList = (child as Element).classList;
+  //   if (childClassList.contains("active-left")) {
+  //     childClassList.remove("active-left");
+  //     childClassList.add("deactive-right");
+  //   } else if (childClassList.contains("active-mid")) {
+  //     childClassList.remove("active-mid");
+  //     childClassList.add("deactive-mid");
+  //   } else if (childClassList.contains("active-right")) {
+  //     childClassList.remove("active-right");
+  //     childClassList.add("deactive-left");
+  //   }
+  // });
+};
+
+const effectChange = (event: Event) => {
+  const target: Element = event.currentTarget as Element;
+  const targetID: string = target.id;
+  const targetIDLength = targetID.length;
+  const targetPosition = targetID[targetIDLength - 1] as directionKeys;
+  const changeDirection = getChangeDirection(targetPosition);
+  const indirectTargetID = targetID.startsWith("card-wrapper-")
+    ? `plans-counter-${targetPosition}`
+    : `card-wrapper-${targetPosition}`;
+  const indirectTarget = get(indirectTargetID) as Element;
+  const targetParent = target.parentElement as HTMLElement;
+  const indirectTargetParent = indirectTarget.parentElement as HTMLElement;
+
+  removeFocusOnPrevTarget(targetParent, changeDirection);
+  removeFocusOnPrevTarget(indirectTargetParent, changeDirection);
+
+  console.log(changeDirection);
+
+  focusOnTarget(target, changeDirection);
+  focusOnTarget(indirectTarget, changeDirection);
+};
+
+const getFocusTargets = (): string[] => {
+  const targets: string[] = [];
   ["plans-counter-", "card-wrapper-"].forEach((classname: string) => {
     SELECTORS.forEach((selctor: string) => {
-      const element: Element | null = document.getElementById(
-        `${classname}${selctor}`
-      );
-      element?.addEventListener("click", changeEffect);
+      targets.push(`${classname}${selctor}`);
     });
   });
+  return targets;
+};
+
+const get = (elementID: string): Element | null => {
+  return document.getElementById(elementID);
+};
+
+export const focusOnHover = (): void => {
+  const targets: string[] = getFocusTargets();
+
+  const elements: Element[] = targets.map(
+    (target: string) => get(target) as Element
+  );
+
+  elements.forEach((element: Element) =>
+    element.addEventListener("click", effectChange)
+  );
 };
