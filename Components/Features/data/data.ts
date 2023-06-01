0a3551bb9dedbade8a1d798cbe054ed3c8f63016
alivarastepour <alivaraste@outlook.com>
2023-06-01 14:16:12 +0330

    a functionallity is added to get partial keys of object in an array

--- Components/Features/data/data.ts
+++ Components/Features/data/data.ts
@@ -1,41 +1,48 @@
-export const data = [
+type Tdata = {
+  id: string;
+  title: string;
+  content: string;
+  className: string;
+};
+
+export const data: Tdata[] = [
   {
-    id: 1,
+    id: "1",
     title: "Manage your tasks in one shared place",
     content:
       "Say goodbye to sticky notes and to-do lists: Trello allows teams of any size to easily manage tasks and hit deadlines - all without ever leaving the app. Trello makes it easy to organize and track individual or team tasks in one place.",
     className: "features-selected",
   },
   {
-    id: 2,
+    id: "2",
     title: "Project management without the mayhem",
     content:
       "From initiation to completion, Trello makes it easy to monitor every aspect of your next project. Designed for teams of any size, anywhere. Manage all of your projects in one powerful tool that makes project management magical.",
     className: "features-not-selected",
   },
   {
-    id: 3,
+    id: "3",
     title: "Meetings should save time, not waste it- Chronos can help",
     content:
       "Meetings should be productive, but done right, they can also boost team morale. Trello creates shared focus and transparency so you can focus on planning for action, getting work done, and celebrating successes.",
     className: "features-not-selected",
   },
   {
-    id: 4,
+    id: "4",
     title: "Joining something new is exciting—we keep it that way!",
     content:
       "Simplify the ramp-up process with a vibrant, visual layout that keeps everything organized and everyone engaged. Trello’s 200+ tool integrations and easy-to-customize boards make every welcome experience feel tailored, without added effort.",
     className: "features-not-selected",
   },
   {
-    id: 5,
+    id: "5",
     title: "Keep information organized and accessible with Chronos",
     content:
       "Knowledge is power - but only if it's accurate, accessible, and used correctly. Give teams a reliable hub from which to contribute, manage, and find relevant information quickly with Chronos. Then, watch their productivity skyrocket!",
     className: "features-not-selected",
   },
   {
-    id: 6,
+    id: "6",
     title:
       "Uncover actionable ideas by brainstorming with Chronos, no matter where you're located",
     content:
@@ -43,3 +50,15 @@ export const data = [
     className: "features-not-selected",
   },
 ];
+
+type TdataKey = keyof Tdata;
+
+export const getData = (keys: TdataKey[]) => {
+  const res = [];
+  for (const item of data) {
+    const temp: any = {};
+    for (const key of keys) temp[key] = item[key];
+    res.push(temp);
+  }
+  return res;
+};
