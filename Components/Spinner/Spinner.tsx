dbe9c6d68d61a3d0ea4e9421d58cbb31bb4d985b
alivarastepour <alivaraste@outlook.com>
2023-06-15 15:42:21 +0330

    improved the Spinner UI

--- Components/Spinner/Spinner.tsx
+++ Components/Spinner/Spinner.tsx
@@ -4,12 +4,22 @@ const Spinner = () => {
   return (
     <>
       <div className={styles["spinner-wrapper"]}>
-        <div
-          className={`${styles["spinner-item-1"]} ${styles["spinner-item"]}`}
-        ></div>
-        <div
-          className={`${styles["spinner-item-2"]} ${styles["spinner-item"]}`}
-        ></div>
+        <div className={styles["spinner-flex-container"]}>
+          <div className={styles["spinner-item-container"]}>
+            <div
+              className={`${styles["spinner-item-1"]} ${styles["spinner-item"]}`}
+            ></div>
+            <div
+              className={`${styles["spinner-item-2"]} ${styles["spinner-item"]}`}
+            ></div>
+            <div
+              className={`${styles["spinner-item-3"]} ${styles["spinner-item"]}`}
+            ></div>
+            <div
+              className={`${styles["spinner-item-4"]} ${styles["spinner-item"]}`}
+            ></div>
+          </div>
+        </div>
       </div>
     </>
   );
