dfe34555c30f794a47a54ba009a071763be83447
alivarastepour <alivaraste@outlook.com>
2023-06-19 11:39:49 +0330

    added meta tag for theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: light)"
+        content="#ffe5e5"
+      />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: dark)"
+        content="#191825"
+      />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
