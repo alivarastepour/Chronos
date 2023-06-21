eca7bbda7b6bbcbd9f8b13b4535e4c06bbbe935f
alivarastepour <alivaraste@outlook.com>
2023-06-21 20:50:12 +0330

    inits for termsOfServices page is added

---
+++ pages/termsOfServices.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const TermsOfServices = () => {
+  return (
+    <>
+      <Head>
+        <title>Chronos | Terms Of Services</title>
+        <meta
+          name="description"
+          content="read our terms of services. you need to agree to them before using our services."
+        />
+      </Head>
+    </>
+  );
+};
+export default TermsOfServices;
